769ac8ae6ba31858b6af8687e156de82f11338d9
Adeline Sparks <adeline.sprks@gmail.com>
2021-04-24 18:50:31 -0700

    Just make the adder tree always signed.

--- vhdl/adder_tree.vhd
+++ vhdl/adder_tree.vhd
@@ -4,41 +4,20 @@ use ieee.numeric_std.all;
 use work.imucam_pkg.all;
 
 entity adder_tree is
-  generic (
-    use_signed : boolean
-  );
   port (
     clk : in std_logic;
-    input : in std_logic_vector_1d;
+    input : in signed_1d;
     input_valid : in std_logic;
-    output : out std_logic_vector;
+    output : out signed;
     output_valid : out std_logic
   );
 end entity;
 
 architecture rtl of adder_tree is
-  function resize(x : std_logic_vector; new_size : natural) return std_logic_vector is
-  begin
-    if use_signed then
-      return std_logic_vector(resize(signed(x), new_size));
-    else
-      return std_logic_vector(resize(unsigned(x), new_size));
-    end if;
-  end function;
-
-  function add(x : std_logic_vector; y : std_logic_vector) return std_logic_vector is
-  begin
-    if use_signed then
-      return std_logic_vector(signed(x) + signed(y));
-    else
-      return std_logic_vector(unsigned(x) + unsigned(y));
-    end if;
-  end function;
-  
   constant depth : natural := log2(input'length);
   constant num_partials : natural := 2 ** depth;
   
-  signal partials : std_logic_vector_2d(depth downto 0)(num_partials-1 downto 0)(output'range);
+  signal partials : signed_2d(depth downto 0)(num_partials-1 downto 0)(output'range);
   signal valids : std_logic_vector(depth downto 0);
 begin
   process (all)
@@ -54,7 +33,7 @@ begin
     if rising_edge(clk) then
       for level in 0 to depth-1 loop
         for i in 0 to 2**(depth-level-1)-1 loop
-          partials(level+1)(i) <= add(partials(level)(i*2), partials(level)(i*2+1));
+          partials(level+1)(i) <= partials(level)(i*2) + partials(level)(i*2+1);
         end loop;
       end loop;
     end if;
--- vhdl/top.vhd
+++ vhdl/top.vhd
@@ -6,9 +6,9 @@ use work.imucam_pkg.all;
 entity top is
   port (
     clk : in std_logic;
-    input : in std_logic_vector_1d(8 downto 0)(3 downto 0);
+    input : in signed_1d(8 downto 0)(3 downto 0);
     input_valid : std_logic;
-    output : out std_logic_vector(7 downto 0);
+    output : out signed(7 downto 0);
     output_valid : out std_logic
   );
 end top;
@@ -16,9 +16,6 @@ end top;
 architecture rtl of top is
 begin
   summation : entity work.adder_tree
-    generic map (
-      use_signed => false
-    )
     port map (
       clk => clk,
       input => input,
